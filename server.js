d35deb27721a348ec96ac523aefacf20aa3d85cd
Mia Adjei <adjei.m@gmail.com>
2021-07-14 10:42:44 -0400

    Create Go Rooms on the server side

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const port = 5000;
 const AccessToken = require('twilio').jwt.AccessToken;
 const VideoGrant = AccessToken.VideoGrant;
 
+const twilioClient = require('twilio')(process.env.TWILIO_API_KEY_SID,
+  process.env.TWILIO_API_KEY_SECRET, { accountSid: process.env.TWILIO_ACCOUNT_SID });
+
 app.use(express.json());
 
 // Serve static files from the public directory
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile('public/index.html');
 });
 
 app.post('/token', async (req, res) => {
@@ -19,29 +22,50 @@ app.post('/token', async (req, res) => {
     return res.status(400);
   }
 
-  // Get the user's identity from the request
+  // Get the user's identity and the room name from the request
   const identity  = req.body.identity;
+  const roomName  = req.body.room;
 
-  // Create a video grant
-  const videoGrant = new VideoGrant({
-    room: req.body.room
-  })
+  try {
+    // See if the room exists already
+    const roomList = await twilioClient.video.rooms.list({uniqueName: roomName, status: 'in-progress'});
 
-  // Create an access token
-  const token = new AccessToken(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_API_KEY_SID,
-    process.env.TWILIO_API_KEY_SECRET,
-  );
+    let room;
 
-  // Add the video grant and the user's identity to the token
-  token.addGrant(videoGrant);
-  token.identity = identity;
+    if (!roomList.length) {
+      // Call the Twilio video API to create the new Go room
+      room = await twilioClient.video.rooms.create({
+        uniqueName: roomName,
+        type: 'go'
+      });
+    } else {
+      room = roomList[0];
+    }
 
-  // Serialize the token to a JWT and return it to the client side
-  res.send({
-    token: token.toJwt()
-  });
+    // Create a video grant for this specific room
+    const videoGrant = new VideoGrant({
+      room: room.uniqueName,
+    })
+
+    // Create an access token
+    const token = new AccessToken(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_API_KEY_SID,
+      process.env.TWILIO_API_KEY_SECRET,
+    );
+
+    // Add the video grant and the user's identity to the token
+    token.addGrant(videoGrant);
+    token.identity = identity;
+
+    // Serialize the token to a JWT and return it to the client side
+    res.send({
+      token: token.toJwt()
+    });
+
+  } catch (error) {
+    res.status(400).send({error});
+  }
 });
 
 // Start the Express server
